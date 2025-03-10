907b15e618dc325c94bfcb630b7a0bdbbef70621
Nguyen Minh Long <nguyenminhlong@luvina.net>
2025-03-10 16:59:35 +0700

    project base

---
+++ src/main/java/com/training/social_app/entity/FriendShip.java
@@ -0,0 +1,33 @@
+package com.training.social_app.entity;
+
+import com.training.social_app.enums.RequestStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "friendships")
+public class FriendShip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    private User user1;
+
+    @ManyToOne
+    private User user2;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name="status")
+    @Enumerated(EnumType.STRING)
+    private RequestStatus requestStatus;
+
+}
---
+++ src/main/java/com/training/social_app/enums/RequestStatus.java
@@ -0,0 +1,5 @@
+package com.training.social_app.enums;
+
+public enum RequestStatus {
+    PENDING, ACCEPTED, REJECTED
+}
