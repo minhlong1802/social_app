2fceabf6b589ada66d3a913ae2a43b017342b548
nguyenminhlong <nguyenlong18022004@gmail.com>
2025-03-19 16:21:12 +0700

    add function generate excel report for excel file

---
+++ src/main/java/com/training/social_app/entity/Report.java
@@ -0,0 +1,48 @@
+package com.training.social_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reports")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Report {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @Column(name = "week_start_date", nullable = false)
+    private LocalDate weekStartDate;
+
+    @Column(name = "week_end_date", nullable = false)
+    private LocalDate weekEndDate;
+
+    @Column(name = "post_count")
+    private Integer postCount = 0;
+
+    @Column(name = "new_friends_count")
+    private Integer newFriendsCount = 0;
+
+    @Column(name = "like_count")
+    private Integer likeCount = 0;
+
+    @Column(name = "comment_count")
+    private Integer commentCount = 0;
+
+    @Column(name= "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name= "updated_at")
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
---
+++ src/main/java/com/training/social_app/service/ReportService.java
@@ -0,0 +1,6 @@
+package com.training.social_app.service;
+
+public interface ReportService {
+    //Generate a weekly report to file excel
+    byte[] generateWeeklyReport();
+}
